3e19d313e43a6041ed2f72cf866e9dcc7563e656
eason <451114984@qq.com>
2017-04-04 09:57:13 +0800

    fix bug

--- controller/salesperson.controller.js
+++ controller/salesperson.controller.js
@@ -1,6 +1,15 @@
 /**
  * Created by eason on 17-4-3.
  */
+function errprint($mdToast,msg){
+    $mdToast.show(
+        $mdToast.simple()
+            .textContent(msg)
+            .position('right start')
+            .hideDelay(500)
+    );
+}
+
 module.exports = function($scope,$mdToast,CalService){
     $scope.currentMonth = 'Jan';
     $scope.months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
@@ -9,42 +18,34 @@ module.exports = function($scope,$mdToast,CalService){
 
     $scope.submit = function(){
         if($scope.report=='-1'){
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent(`Finish a ${$scope.currentMonth} report!`)
-                    .position('right start')
-                    .hideDelay(500)
-            );
+            errprint($mdToast,`Finish a ${$scope.currentMonth} report!`);
             CalService.finish($scope.currentMonth);
             return;
         }
 
+        if(!$scope.report) {
+            errprint($mdToast,'The format of the report is incorrect！');
+            return;
+        }
+
         let data = $scope.report.split(',');
+        if(data.length!=3) {
+            errprint($mdToast,'The format of the report is incorrect！');
+            return;
+        }
+
         data[0] = Number(data[0]);
         data[1] = Number(data[1]);
         data[2] = Number(data[2]);
-        if(data.length!=3||isNaN(data[0])||isNaN(data[1])||isNaN(data[2]))
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent('The format of the report is incorrect！')
-                    .position('right start')
-                    .hideDelay(500)
-            );
+        if(isNaN(data[0])||isNaN(data[1])||isNaN(data[2])) {
+            errprint($mdToast,'The format of the report is incorrect！');
+            return;
+        }
 
         if(!CalService.addReport(data))
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent('locks up to 70/stocks up to 80/barries up to 90 per month!')
-                    .position('right start')
-                    .hideDelay(500)
-            );
+            errprint($mdToast,'locks up to 70/stocks up to 80/barries up to 90 per month!');
         else{
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent('Success!')
-                    .position('right start')
-                    .hideDelay(500)
-            );
+            errprint('Success!');
             $scope.report = '00,00,00';
         }
     };
